3fbe2b123a90037b9af87cf8b6b78def89cc7acc
Edw <edw-toni@hotmail.com>
2023-04-03 22:09:19 -0500

    Storage api

--- app/services/BooksService.php
+++ app/services/BooksService.php
@@ -7,6 +7,8 @@ class BooksService {
     public function getFileRoute($id)
     {
         $file = 'C:/xampp/htdocs/CoolDocs/storage/app/books/4/book/6eoQXeIx3t39mhNSigD8VF7MzCXXAefzZ1bamC3E.pdf';
+        $file = '/webapps/apps/puente-digital/backend/storage/app/books/73/book/1657040558.pdf';
+        
         if(!file_exists($file)){
             responseJson(['message'=> 'Libro no encontrado'], 404);
             exit;
